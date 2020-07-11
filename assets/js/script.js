33558964d9f7a9567feb3d351d55aa99bcef4449
allielynne20 <allielynne20@gmail.com>
2020-07-10 22:25:00 -0400

    got the 5 day forecast to work! and it dispays the searched cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
+var currentDay = document.getElementById("currentDay");
+currentDay.innerHTML = moment().format('MM/DD/YYYY');
+
 //this variable targets the id of hte input that contains the searched city
 var cityInput = document.getElementById("cityname");
 //this variable targets the id of the entire form
 // var cityForm = document.querySelector("#city-form");
 //this variable targets the id of the empty div under the form 
-// var historyList = document.querySelector("#city-history");
+var historyList = document.querySelector("#city-history");
+
 $("#button").on("click", function () {
     event.preventDefault();
     var cityname = $("#cityname").val();
@@ -34,60 +38,74 @@ var weatherSearch = function (cityname) {
             // var uv = $("<h4>").attr("id", "uv");
             var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
 
-            title.append(img);
-            cardBody.append(temp, humid, wind);
+            cardBody.append(img, temp, humid, wind);
             card.append(title, cardBody);
             $("#today").append(card);
-            fiveDay(cityname);
+
+            var lattitude = response.coord.lat;
+            var longitude = response.coord.lon;
+
+            fiveDay(lattitude, longitude);
+
         })
 }
 
+var fiveDay = function (lattitude, longitude) {
 
-var fiveDay = function (cityname) {
-    var weatherCondition = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=fe4831ea04035583e11d878e5799c1d9&units=imperial';
+    // var weatherCondition = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=fe4831ea04035583e11d878e5799c1d9&units=imperial';
+    // $.ajax({
+    //     type: "GET",
+    //     url: weatherCondition,
+    //     dataType: "json",
+    // })
+
+    var weatherCondition = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lattitude + '&lon=' + longitude + '&exclude=minutely,hourly&appid=fe4831ea04035583e11d878e5799c1d9&units=imperial';
+
+    console.log(weatherCondition)
+
     $.ajax({
         type: "GET",
         url: weatherCondition,
         dataType: "json",
     })
-    .then(function(response) {
-        console.log(response); 
-        $("#forecast").empty();
 
-        for (i=0; i < response.list.length; i++) {
-            if (response.list[i].dt_txt.indexOf("8:00:00") !== -1) {
+        .then(function (response) {
+            console.log(response);
 
-                console.log(response.list[0].dt_txt.indexOf("08:00:00"));
+            $("#forecast").empty();
 
-                var card = $('<div>').addClass("card text-white bg-info mb-3 col");
-                var cardBody = $('<div>').addClass("card-body");
-                var date = $("<h5>").addClass("date-forecast").text(response.list[i].dt_txt);
-                var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.list[i].weather.icon + ".png");
-                var temp = $("<h5>").attr("id", "temp").addClass("date-forecast").text(response.list[i].main.temp);
-                var humid = $("<h5>").attr("id", "humid").text(response.list[i].main.humidity);
+            for (i = 0; i < response.daily.length; i++) {
+                if (response.daily[i]) {
 
-                card.append(cardBody);
-                cardBody.append(data, img, temp, humid);
-                $("#forecast").append(card);
-                
+                    var card = $('<div>').addClass("card text-white bg-info mb-3 col");
+                    var cardBody = $('<div>').addClass("card-body row");
+                    var date = $("<h5>").addClass("date-forecast").html(response.daily[i].dt);
+                    var img = $("<img>").addClass("imageicon").attr("src", "https://openweathermap.org/img/w/" + response.daily[i].weather[0].icon + ".png");
+                    var temp = $("<h5>").attr("id", "temp").addClass("date-forecast").text(response.daily[i].temp.day);
+                    var humid = $("<h5>").attr("id", "humid").text(response.daily[i].humidity);
+
+                    card.append(cardBody);
+                    cardBody.append(date, img, temp, humid);
+                    $("#forecast").append(card);
+
+                }
             }
-        }
-    })
+        })
 }
 
 
-//make a fetch request including the variable in the query url to get the weather for the city that was searched 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=locust,q=nc&appid=fe4831ea04035583e11d878e5799c1d9')
+$("#button").on("click", function () {
+    var cityname = $("#cityname").val();
+    var cityHistory = $("#city-history");
+    citySubmit = $("<div>").attr("id", "citySubmit").text(cityname);
+    cityHistory.append(citySubmit);
+    localStorage.setItem("cityname", cityname);
+})
 
-//         .then(function (response) {
-//             return response.json();
-//         })
-// }
-var getWeatherCondition = function (name) {
-    var weatherCondition = 'http://api.openweathermap.org/data/2.5/weather' + name + '/appid=fe4831ea04035583e11d878e5799c1d9';
-
-    // fetch ('http://api.openweathermap.org/data/2.5/weather?q=locust,q=nc&appid=fe4831ea04035583e11d878e5799c1d9')
-
-    fetch(weatherCondition);
-    console.log(weatherCondition);
-}
+$("#citySubmit").on("click", function () {
+    $("#cityname").html("cityname");
+    localStorage.getItem("cityname");
+    
+    // cityname.value = "";
+    // weatherSearch(cityname);
+})
